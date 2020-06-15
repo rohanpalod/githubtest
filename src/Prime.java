acf6ec96c296ab9c78586ce3ed37e4a1e9edf6a4
rohitpalod <palodrht@gmail.com>
2020-06-15 14:16:14 +0530

    Minor change in primeclasss

--- src/Prime.java
+++ src/Prime.java
@@ -10,6 +10,7 @@ public class Prime {
             System.out.println("Number entered by you is zero, which is neither prime nor a composite number");
         if(number==1)
             System.out.println("Number entered by you is one, which is not a prime number but a composite number");
+	if(true){}
         Boolean isPrime=false;
         if(number>2){
             for(int i=2; i<=number-1; i++){
